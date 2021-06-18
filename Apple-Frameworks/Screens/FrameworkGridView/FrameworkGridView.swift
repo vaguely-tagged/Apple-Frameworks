4be8be8aaf8041cadc6454a29b3dba0006451f80
vaguely-tagged <sgherzivincent@gmail.com>
2021-06-18 18:02:56 +0900

    apple frameworks commit

---
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -0,0 +1,41 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Vince Sgherzi on 6/16/21.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: viewModel.columns){
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                viewModel.selectedFramework = framework
+                            }
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+            .sheet(isPresented: $viewModel.isShowingDetailView){
+                FrameworkDetailView(framework: viewModel.selectedFramework!, isShowingDetailView: $viewModel.isShowingDetailView)
+            }
+        }
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+
